c748885ddfbf0ec163cc59ba3da3e3726dd8a844
slw13d-ly <jinye24@naver.com>
2026-02-12 15:59:44 +0900

    py05_입출력제어

--- jupy.sh
+++
@@ -1,4 +0,0 @@
-echo "주피터랩 서버를 시작합니다."
-source /home/jh/venv/bin/activate
-jupyter lab
-
--- test.py
+++
@@ -1,6 +0,0 @@
-import time as t
-print('배고프다', end='\r')
-t.sleep(1)
-print('근데 ',end='\r')
-t.sleep(1)
-print('시간이 많이 남았어...')
