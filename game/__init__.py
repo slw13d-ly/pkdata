c23da80ddb55c46d073bd89a8541bdea8cc8d450
slw13d-ly <jinye24@naver.com>
2026-02-19 12:08:34 +0900

    26/2/19 수정

---
+++ game/__init__.py
@@ -0,0 +1,8 @@
+from .graphic.render import render_test
+
+VERSION = 4.7  
+    
+def print_version_info():
+    print(f"The version of this game is {VERSION}.")
+
+print("Initializing game ...")
---
+++ mod1.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+
+if __name__ == "__main__": # 직접 실행할 때만, 실행해라
+    print(add(1, 4))
+    print(sub(4, 2))
